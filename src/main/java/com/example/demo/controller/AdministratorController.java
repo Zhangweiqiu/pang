3a73c5d92e634512c1c32ea1f11fe1a0417cc372
系小吴先森 <1036383548@qq.com>
2018-09-06 16:44:40 +0800

    1

--- src/main/java/com/example/demo/controller/AdministratorController.java
+++ src/main/java/com/example/demo/controller/AdministratorController.java
@@ -10,11 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
 import com.example.demo.pojo.Administor;
-import com.example.demo.pojo.Kefu;
 import com.example.demo.repository.AdministratorRepository;
-import com.example.demo.repository.KefuRepository;
 import com.example.demo.service.AdministratorService;
-import com.example.demo.service.KefuService;
 
 
 @RestController
@@ -26,11 +23,7 @@ public class AdministratorController {
 	@Autowired
 	AdministratorRepository administratorRepository;
 	
-	@Autowired
-	KefuService kefuService;
 	
-	@Autowired
-	KefuRepository kefuRepository;
 	
 	@RequestMapping("/login")
 	public Map<String,Object> checklogin(String aid, String password){
@@ -38,14 +31,7 @@ public class AdministratorController {
 		return map;
 	}
 	
-	@RequestMapping("/addkefu")
-	public boolean addkefu(String name){
-		if(kefuRepository.findByKname(name) != null)
-			return false;
-		Kefu kefu = new Kefu();
-		kefu.setKname(name);
-		return kefuService.save(kefu);
-	}
+	
 	
 	@RequestMapping("/addAdmin")
 	public boolean addAdmin(String name,String aid){
@@ -99,40 +85,6 @@ public class AdministratorController {
         return jsonObject;
 	}	
 	
-	@RequestMapping("/showKefuList")
-	public JSONObject showKefuList(Integer limit, Integer offset) {
-		JSONObject jsonObject = new JSONObject();
-		List<Kefu> KefuList = kefuService.showKefuList();
-		int sie = limit * offset;
-		List<Kefu> KefuList1 = new ArrayList<>();
-		if (KefuList.size() > sie){
-            System.out.println(limit+"========================="+offset);
-            int k = KefuList.size()-sie;
-            if (k > limit)
-                for (int i = 0; i < limit;i++)
-                	KefuList1.add(KefuList.get(i+sie));
-            else
-                for (int i = 0 ; i < k; i++)
-                	KefuList1.add(KefuList.get(i+sie));
-        }else{
-            System.out.println(limit+"========================="+offset);
-            int j = limit*(offset);
-            int k = KefuList.size() - j;
-            for (int i = 0 ; i < k ; i++)
-            	KefuList1.add(KefuList.get(i+j));
-        }
-        jsonObject.put("total",KefuList.size());
-        jsonObject.put("rows",KefuList1);
-        
-        return jsonObject;
-	}	
 	
-	@RequestMapping("/deleteKefu")
-	public boolean deleteKefu(Integer pid) {
-		if(kefuRepository.deleteByMyId(pid) == 1) {
-			return true;
-		}else
-			return false;
-	}
 
 }
---
+++ src/main/java/com/example/demo/controller/KefuController.java
@@ -0,0 +1,78 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.demo.pojo.Kefu;
+import com.example.demo.repository.KefuRepository;
+import com.example.demo.service.KefuService;
+
+@RestController
+public class KefuController {
+	@Autowired
+	KefuService kefuService;
+	
+	@Autowired
+	KefuRepository kefuRepository;
+	
+	@RequestMapping("/addkefu")
+	public boolean addkefu(String name){
+		if(kefuRepository.findByKname(name) != null)
+			return false;
+		Kefu kefu = new Kefu();
+		kefu.setKname(name);
+		return kefuService.save(kefu);
+	}
+	
+	@RequestMapping("/showKefuList")
+	public JSONObject showKefuList(Integer limit, Integer offset) {
+		JSONObject jsonObject = new JSONObject();
+		List<Kefu> KefuList = kefuService.showKefuList();
+		int sie = limit * offset;
+		List<Kefu> KefuList1 = new ArrayList<>();
+		if (KefuList.size() > sie){
+            System.out.println(limit+"========================="+offset);
+            int k = KefuList.size()-sie;
+            if (k > limit)
+                for (int i = 0; i < limit;i++)
+                	KefuList1.add(KefuList.get(i+sie));
+            else
+                for (int i = 0 ; i < k; i++)
+                	KefuList1.add(KefuList.get(i+sie));
+        }else{
+            System.out.println(limit+"========================="+offset);
+            int j = limit*(offset);
+            int k = KefuList.size() - j;
+            for (int i = 0 ; i < k ; i++)
+            	KefuList1.add(KefuList.get(i+j));
+        }
+        jsonObject.put("total",KefuList.size());
+        jsonObject.put("rows",KefuList1);
+        
+        return jsonObject;
+	}	
+	
+	@RequestMapping("/deleteKefu")
+	public boolean deleteKefu(Integer pid) {
+		if(kefuRepository.deleteByMyId(pid) == 1) {
+			return true;
+		}else
+			return false;
+	}
+	
+	@RequestMapping("/getKefus")
+	public Map<String,Object> getKefus() {
+		Map<String,Object> map = new HashMap<>();
+		List<Kefu> list = kefuService.showKefuList();
+		map.put("list", list);
+		map.put("size", list.size());
+		return map;
+	}
+}
