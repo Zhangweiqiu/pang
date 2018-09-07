ae8653da2d94b3c5001562a1429fd3b700c4249a
系小吴先森 <1036383548@qq.com>
2018-09-07 18:34:13 +0800

    1

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -1,18 +1,27 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.example.demo.interceptor.MyInterceptor;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer{
+public class WebMvcConfig extends WebMvcConfigurerAdapter implements WebMvcConfigurer{
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// TODO Auto-generated method stub
-		registry.addInterceptor(new MyInterceptor()).addPathPatterns("/*").excludePathPatterns("/passtopay").excludePathPatterns("/payNotify.do").excludePathPatterns("/login").excludePathPatterns("/index.html").excludePathPatterns("/pay.html").excludePathPatterns("/goBack.html");
+		registry.addInterceptor(new MyInterceptor()).addPathPatterns("/*").excludePathPatterns("/passtopay").excludePathPatterns("/payNotify.do").excludePathPatterns("/login").excludePathPatterns("/admin.html").excludePathPatterns("/index.html").excludePathPatterns("/goBack.html");
 	}
 	
+	 @Override
+     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/admin");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    } 
 }
--- src/main/java/com/example/demo/controller/GoodsNameController.java
+++ src/main/java/com/example/demo/controller/GoodsNameController.java
@@ -23,4 +23,5 @@ public class GoodsNameController {
 	    }else
 	    	return false;
 	}
+	
 }
